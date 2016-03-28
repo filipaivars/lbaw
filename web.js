c0d234e9b1415bb5fca84983b141ecfe7c338c45
filipaivars <ivars.filipa@gmail.com>
2016-03-28 05:02:06 +0100

    hsgfahj

--- web.js
+++ web.js
@@ -4,4 +4,7 @@ var gzippo = require('gzippo');
 
   app.use(express.logger('dev'));
   app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-  app.listen(process.env.PORT || 3000);
+  app.listen(process.env.PORT || 5000);
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
